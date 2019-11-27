6aee131630ecd6a74e70c813df976d9cdcf5de2a
AChelyadinskiy <chelyadinskiy@gmail.com>
2019-11-27 21:46:49 +0200

    modify message.py in views, make new branch

--- src/views/message.py
+++ src/views/message.py
@@ -11,19 +11,6 @@ from schemas import MessageCreateResponseSchema
 from utils.auth import access_token_required
 
 
-async def one():
-    await asyncio.sleep(5)
-    return 1
-
-
-async def two():
-    return 2
-
-
-async def three():
-    return 3
-
-
 @docs(
     tags=['SMS'], summary='Запрос отправки сообщения', description='Описание запроса',
     parameters=APISPEC_DEFAULT_PARAMS,
@@ -43,8 +30,6 @@ async def send_message(request):
         text=request['data']['text'],
         messageId=message_id,
     )
-
-    dummy = await asyncio.gather(one(), two(), three())
 
     validated_games_list = MessageCreateResponseSchema().load(res)
 
